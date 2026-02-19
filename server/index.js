4c963c658a157dd7107d34e5d1823ed2b8815cdc
Supreet Singh <singh04supreet@gmail.com>
2026-02-19 14:08:53 +0530

    Final Commit

--- server/index.js
+++ server/index.js
@@ -9,7 +9,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: [
+        "http://localhost:5173",
+        "https://expensetracker-seven-rho.vercel.app"
+    ],
+    credentials: true
+}));
 app.use(express.json());
 
 // Routes
